a7e48fdc9e145f82f514c51786ecd4fa649d1f31
Arnaud Bossobita <anaudboss@outlook.com>
2026-04-04 18:21:25 -0600

    DOM manipulation with Refs

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,8 +1,9 @@
-import {useEffect, useState} from 'react'
+import {useEffect, useRef, useState} from 'react'
 
 const Timer = () => {
     const [time, setTime] = useState<number>(0);
     const [isActtive, setIsActive] = useState<boolean>(false);
+    const inputRef = useRef<HTMLInputElement>(null);
 
     // If provided empty array, then the effect will only run once when the component mounts
     useEffect(() => {
@@ -25,9 +26,20 @@ const Timer = () => {
         }
     }, [time, isActtive]);
 
+    // Whenever we mounting the component, we want to focus on the input field. 
+    // We can achieve this by using the useRef hook to create a reference to the input element and then 
+    // calling the focus method on it inside a useEffect hook that runs when the component mounts.
+    useEffect(() => {
+        if (inputRef.current) {
+            inputRef.current.placeholder = 'Mounted';
+            inputRef.current.focus();
+        }
+    }, []);
+
   return (
     <>
         <h2>Timer</h2>
+        <input ref={inputRef} placeholder='Input' />
         <span>Elapse Time: {time}</span>
         <button onClick={() => { setIsActive(!isActtive); setTime(time + 1); }}> {!isActtive ? 'Start' : 'Pause'}</button>
         <button onClick={() => { setTime(0); }}>Reset</button>
